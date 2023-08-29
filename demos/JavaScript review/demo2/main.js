5418b878d1961e04fb40f2aecfb13ef916caef1c
Jacob Poncher <jacob_poncher@insideranken.org>
2023-08-29 11:49:51 -0500

    demos

---
+++ demos/JavaScript review/demo2/main.js
@@ -0,0 +1,38 @@
+let stuff = 'thigs';
+
+window.alert('hello');
+console.log('hello world');
+
+const myUser = {
+  firstName : "Jacob",
+  lastName: "Poncher",
+  age: 23
+}
+
+let thing;
+let thing2 = "123";
+
+const x = BigInt(Number.MAX_SAFE_INTEGER + 1);
+
+
+
+const myStudentAges = [18,20,38,55,16]
+
+function Foo(){
+ const arr = ['cat' , 'dog', 'unicorn'];
+
+ const user = { firstName: "jacob ", lastName : "poncher"}
+
+ let i= 0 ;
+
+ for (const key in user) {
+  console.log(key);
+ }
+}
+
+
+
+function Bar(){
+  console.bar('bar')
+}
+Foo();
